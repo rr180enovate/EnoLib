d0663dbaf1f9c13dc29c72846c02483fe6976fe1
Andrei <andrei@Rahul.local>
2017-03-21 18:51:56 +0530

    new create pod

---
+++ Example/Pods/Target Support Files/EnoLib/EnoLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EnoLib : NSObject
+@end
+@implementation PodsDummy_EnoLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-EnoLib_Example/Pods-EnoLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EnoLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_EnoLib_Example
+@end
